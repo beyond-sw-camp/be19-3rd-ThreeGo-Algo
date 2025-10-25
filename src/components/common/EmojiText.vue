fb5083051f3dfcdd947ba3a3e6de77fc250643bc
jiruen <kjr2699@naver.com>
2025-10-25 19:15:47 +0900

    feat: #4 - EmojiText 컴포넌트 구현

---
+++ src/components/common/EmojiText.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="emoji-text">
+        <img :src="emojiType" />
+        <span>{{ count }}</span>
+    </div>
+</template>
+
+<script setup>
+import { emojiImages } from '@/constants/EmojiImages.js';
+import { defineProps, computed } from 'vue';
+
+const props = defineProps({
+    type: String,
+    count: Number
+});
+
+const emojiType = computed(() => emojiImages[props.type]);
+
+</script>
+
+<style scoped>
+.emoji-text {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+}
+
+.emoji-text span {
+    color: #838383;
+    font-size: 13px;
+}
+
+.emoji-text img {
+    width: 20px;
+}
+</style>
