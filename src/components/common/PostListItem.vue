046b4ec6427f831859b6270acbbc011b444c0476
jiruen <kjr2699@naver.com>
2025-10-25 19:36:19 +0900

    feat: #4 - PostListItem 컴포넌트 구현

---
+++ src/components/common/PostListItem.vue
@@ -0,0 +1,106 @@
+<template>
+    <div class="post-item">
+        <div class="post-header">
+            <div class="post-title-area">
+                <p class="post-title" @click="goToPost">
+                    {{ title }}
+                </p>
+                <img v-if="status === 'APPROVED'" :src="certificationIcon" class="certification-icon" />
+            </div>
+            <span class="post-date">{{ createdAt }}</span>
+        </div>
+
+        <MiniProfile :nickname="nickname" :rankName="rankName" />
+
+        <div class="post-footer">
+            <EmojiText v-if="likeCount != undefined" type="grayLike" :count="likeCount" />
+            <EmojiText type="grayComment" :count="commentCount" />
+            <EmojiText v-if="peopleCount != undefined" type="grayPeople" :count="peopleCount" />
+        </div>
+    </div>
+</template>
+
+<script setup>
+import certificationImage from '@/assets/icons/certification.svg';
+import MiniProfile from '@/components/common/MiniProfile.vue'
+import EmojiText from '@/components/common/EmojiText.vue';
+import { defineProps } from 'vue'
+
+const props = defineProps({
+    id: { type: Number, required: true },
+    title: { type: String, required: true },
+    nickname: { type: String, required: true },
+    rankName: { type: String, required: true },
+    createdAt: { type: String, required: true },
+    status: { type: String, default: '' },
+    likeCount: Number,
+    commentCount: { type: Number, required: true },
+    peopleCount: Number
+});
+
+const certificationIcon = certificationImage;
+
+// TODO 게시물 상세 페이지로 이동 기능 구현
+const goToPost = () => {
+    console.log('게시물 상세 페이지로 이동 예정!');
+}
+</script>
+
+<style scoped>
+.post-item {
+    display: flex;
+    flex-direction: column;
+    padding: 12px 0;
+    gap: 7px;
+    font-family: 'Noto Sans KR', sans-serif;
+    padding: 13px 10px;
+}
+
+.post-item:hover {
+    background-color: #F5F5F5;
+}
+
+.post-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.post-title-area {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    height: 22px;
+}
+
+.post-title {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 14px;
+    cursor: pointer;
+}
+
+.post-title:hover {
+    color: #0AA2EB;
+    text-decoration: underline;
+}
+
+.certification-icon {
+    width: 22px;
+}
+
+.post-date {
+    font-size: 13px;
+    color: #555;
+}
+
+.post-footer {
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+    gap: 8px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '@/views/HomeView.vue';
 import ButtonDemoPage from '@/views/demo/ButtonDemoPage.vue';
 import MiniProfileDemoPage from '@/views/demo/MiniProfileDemoPage.vue';
+import PostListItemDemoPage from '@/views/demo/PostListItemDemoPage.vue';
 
 const routes = [
     { path: '/', component: HomeView },
     { path: '/demo/button', component: ButtonDemoPage },
-    { path: '/demo/mini-profile', component: MiniProfileDemoPage }
+    { path: '/demo/mini-profile', component: MiniProfileDemoPage },
+    { path: '/demo/post-list-item', component: PostListItemDemoPage}
 ]
 
 const router = createRouter({
