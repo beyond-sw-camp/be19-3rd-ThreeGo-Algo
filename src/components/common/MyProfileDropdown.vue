bf2d2a4f06bbdbfaf504ab69cce787930f76f493
HoodRyan <bty0823@naver.com>
2025-10-25 23:01:31 +0900

    fix: #2 -  MyProfileDropdown 아이콘->svg 파일로 교체

--- src/components/common/MyProfileDropdown.vue
+++ src/components/common/MyProfileDropdown.vue
@@ -1,26 +1,40 @@
 <template>
   <el-dropdown trigger="click" class="profile-dropdown">
     <span class="el-dropdown-link">
-      <el-icon><User /></el-icon>
-      <span class="ml-1">{{ userName || '내 정보' }}</span>
+      <img src="@/assets/icons/myicon.svg" alt="myinfo" class="my-information">
+      <span class="ml-1">내 정보</span>
     </span>
 
     <template #dropdown>
       <el-dropdown-menu class="dropdown-menu">
         <!-- 마이페이지 -->
-        <el-dropdown-item @click="handleMyPage">
-          <el-icon><User /></el-icon>
-          <span>{{ myPageText }}</span>
+        <el-dropdown-item @click="goMyPage">
+          <img src="@/assets/icons/myicon.svg" alt="mypage" class="mypage-icon">
+          <span>마이페이지</span>
         </el-dropdown-item>
 
         <!-- 로그아웃 -->
         <el-dropdown-item divided @click="openLogoutModal">
-          <el-icon><SwitchButton /></el-icon>
-          <span>{{ logoutText }}</span>
+          <img src="@/assets/icons/logout.svg" alt="logout" class="logout-icon" />
+          <span>로그아웃</span>
         </el-dropdown-item>
       </el-dropdown-menu>
     </template>
   </el-dropdown>
+
+  <!-- 로그아웃 다이얼로그 -->
+  <el-dialog
+    v-model="logoutDialog"
+    title="로그아웃 하시겠습니까?"
+    width="320px"
+  >
+    <span>정말 로그아웃하시겠습니까?</span>
+    <template #footer>
+      <el-button @click="logoutDialog = false">취소</el-button>
+      <el-button type="danger" @click="logout">확인</el-button>
+    </template>
+  </el-dialog>
+
 
   <!-- 로그아웃 팝업 -->
   <el-dialog
