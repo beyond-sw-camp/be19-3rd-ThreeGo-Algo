59cfceb42f2efa2dae4f3546dcae6f88ef52a5ec
jiruen <kjr2699@naver.com>
2025-10-26 23:30:12 +0900

    feat: #11 - 알고리즘 퀴즈 페이지 구현

--- src/router/index.js
+++ src/router/index.js
@@ -21,6 +21,8 @@ import OneButtonPopupDemo from '@/views/demo/OneButtonPopupDemo.vue';
 import RoadmapListItemDemoPage from '@/views/demo/RoadmapListItemDemoPage.vue';
 import AlgoPostListItemDemoPage from '@/views/demo/AlgoPostListItemDemoPage.vue';
 import AlgoMainView from '@/views/AlgoMainView.vue';
+import AlgoQuizDemoPage from '@/views/demo/AlgoQuizDemoPage.vue';
+import AlgoQuizView from '@/views/AlgoQuizView.vue';
 
 const routes = [
   // 기본 홈
@@ -45,11 +47,13 @@ const routes = [
   { path: '/demo/roadmap/:roadmapId', component: RoadmapListItemDemoPage},
   { path: '/demo/roadmap', redirect: '/demo/roadmap/1'},
   { path: '/demo/algo-post-list-item', component: AlgoPostListItemDemoPage },
+  { path: '/demo/algo/quiz', component: AlgoQuizDemoPage },
 
   // 알고리즘 학습 라우트
   { path: '/algorithm/roadmap/:roadmapId', component: AlgoMainView},
   { path: '/algorithm/roadmap', redirect: '/algorithm/roadmap/1'},
-  { path: '/algorithm', redirect: '/algorithm/roadmap/1'}
+  { path: '/algorithm', redirect: '/algorithm/roadmap/1'},
+  { path: '/algorithm/post/:postId/quiz/:quizId', component: AlgoQuizView},
 ]
 
 const router = createRouter({
