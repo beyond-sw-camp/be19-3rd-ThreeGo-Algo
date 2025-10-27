52bb7fe4821b374ff9be014084446fea807164df
김민지 <ghi512151@gmail.com>
2025-10-27 13:58:18 +0900

    feat: #24 - 모든 api 요청에 토큰 자동 첨부

--- src/api/coreApi.js
+++ src/api/coreApi.js
@@ -5,4 +5,16 @@ const coreApi = axios.create({
     withCredentials: true,
 })
 
+// 모든 요청에 토큰 자동 첨부
+coreApi.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('accessToken')
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`
+        }
+        return config
+    },
+    (error) => Promise.reject(error)
+)
+
 export default coreApi
--- src/api/memberApi.js
+++ src/api/memberApi.js
@@ -5,4 +5,16 @@ const memberApi = axios.create({
     withCredentials: true,
 })
 
+// 모든 요청에 토큰 자동 첨부
+memberApi.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('accessToken')
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`
+        }
+        return config
+    },
+    (error) => Promise.reject(error)
+)
+
 export default memberApi
